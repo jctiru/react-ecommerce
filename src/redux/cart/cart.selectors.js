03b8e9b0ddf7cb9e7faeb024061b17c00d3d0092
jctiru <jonathanchristiantiru@gmail.com>
2019-12-27 18:11:41 +1000

    Add checkout table header and total

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -21,3 +21,11 @@ export const selectCartItemsCount = createSelector(
       0
     )
 );
+
+export const selectCartTotal = createSelector([selectCartItems], cartItems =>
+  cartItems.reduce(
+    (accumulatedTotal, cartItem) =>
+      accumulatedTotal + cartItem.quantity * cartItem.price,
+    0
+  )
+);
