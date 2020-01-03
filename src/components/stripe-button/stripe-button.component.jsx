e85a81882c4f07d079e23556ba2d4d1ab4916393
jctiru <jonathanchristiantiru@gmail.com>
2020-01-03 12:34:49 +1000

    Add code to send stripe to backend

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 import Logo from "../../assets/crown.svg";
 
@@ -7,8 +8,24 @@ const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_k0zjPzo8Fm45BDMd2HBZ9sZP005Wtc69HN";
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        token,
+        amount: priceForStripe
+      }
+    })
+      .then(response => {
+        console.log("Payment successful: ", response);
+        alert("Payment successful");
+      })
+      .catch(error => {
+        console.log("Payment failure: ", error);
+        alert(
+          "There was an issue with your payment. Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
